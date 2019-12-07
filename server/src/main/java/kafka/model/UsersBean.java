91eecbec9796bc99fe1315993fe733d446c4efdc
rahul <rahulbats@gmail.com>
2019-12-06 19:10:10 -0600

    changes

--- server/src/main/java/kafka/model/UsersBean.java
+++ server/src/main/java/kafka/model/UsersBean.java
@@ -4,27 +4,43 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Component
 public class UsersBean {
 
-    @Value("${allowedusers}")
+    @Value("${allowedusers:#{null}}")
     private String allowedUsers;
 
-    Map<String, String> userPasswords = new HashMap<>();
+    @Value("${allowedusersProp:#{null}}")
+    private String allowedUsersProp;
 
+    //Map<String, String> userPasswords = new HashMap<>();
+    Properties userProps = new Properties();
     @PostConstruct
-    public void init() {
-        String[] userPasswordPairs = allowedUsers.split(",");
-        for(String userPasswordPair: userPasswordPairs) {
-            String[] userPassword = userPasswordPair.split(":");
-            userPasswords.put(userPassword[0], userPassword[1]);
+    public void init() throws IOException {
+        if(allowedUsersProp!=null) {
+            InputStream inputStream  = new FileInputStream(allowedUsersProp);
+            userProps.load(inputStream);
+        } else {
+            String[] userPasswordPairs = allowedUsers.split(",");
+            for(String userPasswordPair: userPasswordPairs) {
+                String[] userPassword = userPasswordPair.split(":");
+                userProps.put(userPassword[0], userPassword[1]);
+                //userPasswords.put(userPassword[0], userPassword[1]);
+            }
         }
+
+
     }
 
     public String getPassword(String username) {
-        return userPasswords.get(username);
+
+        return userProps.get(username).toString();
     }
 }
--- server/src/main/java/kafka/service/KafkaMessageService.java
+++ server/src/main/java/kafka/service/KafkaMessageService.java
@@ -31,6 +31,26 @@ public class KafkaMessageService {
     @Value("${schema.registry.url}")
     private String schemaRegistryURL;
 
+    @Value("${ssl.truststore.location}")
+    private String truststoreLocation;
+
+    @Value("${ssl.keystore.location}")
+    private String keystoreLocation;
+
+    @Value("${ssl.truststore.password}")
+    private String truststorePassword;
+
+    @Value("${ssl.keystore.password}")
+    private String keystorePassword;
+
+    @Value("${ssl.key.password}")
+    private String keyPassword;
+
+    @Value("${sasl.mechanism}")
+    private String saslMechanism;
+
+
+
     public List<Message> listMessages(String username, String password, String topic, int partition,
                                       int maxMessagesToReturn) throws KafkaException {
         Map<String, List<PartitionInfo>> topics;
@@ -64,17 +84,29 @@ public class KafkaMessageService {
             valueType =  MessageType.valueOf("JSON");
         }
 
+        String jaasString = saslMechanism.equals("PLAIN")?
+                "org.apache.kafka.common.security.plain.PlainLoginModule required  username=\""+username+"\"  password=\""+password+"\";":
+                "org.apache.kafka.common.security.scram.ScramLoginModule required  username=\""+username+"\"  password=\""+password+"\";";
 
         Properties props = new Properties();
         props.put("bootstrap.servers", bootstrap_servers);
         props.put("group.id", "kafka-ui-consumer-group");
         props.put("security.protocol", security_protocol);
-        props.put("sasl.mechanism","PLAIN");
-        props.put("sasl.jaas.config","org.apache.kafka.common.security.plain.PlainLoginModule required  username=\""+username+"\"  password=\""+password+"\";");
+        props.put("sasl.mechanism",saslMechanism);
+        props.put("sasl.jaas.config",jaasString);
         props.put("key.deserializer", keyType==MessageType.JSON? "org.apache.kafka.common.serialization.StringDeserializer": KafkaAvroDeserializer.class.getName());
         props.put("value.deserializer", valueType==MessageType.JSON? "org.apache.kafka.common.serialization.StringDeserializer": KafkaAvroDeserializer.class.getName());
         props.put("schema.registry.url", schemaRegistryURL);
-
+        if(truststoreLocation!=null)
+            props.put("ssl.truststore.location", truststoreLocation);
+        if(keystoreLocation!=null)
+            props.put("ssl.keystore.location", keystoreLocation);
+        if(truststorePassword!=null)
+            props.put("ssl.truststore.password", truststorePassword);
+        if(keystorePassword!=null)
+            props.put("ssl.keystore.password", keystorePassword);
+        if(keyPassword!=null)
+            props.put("ssl.key.password", keyPassword);
 
         KafkaConsumer<Object, Object> consumer = new KafkaConsumer<Object, Object>(props);
 
@@ -84,9 +116,13 @@ public class KafkaMessageService {
         consumer.assign(topicPartitions);
         consumer.seekToEnd(topicPartitions);
         long endPosition = consumer.position(topicPartition);
+
+        consumer.seekToBeginning(topicPartitions);
+        long beginningPosition = consumer.position(topicPartition);
+
         long recentMessagesStartPosition = endPosition - maxMessagesToReturn;
-        if(recentMessagesStartPosition<0)
-            recentMessagesStartPosition =0;
+        if(recentMessagesStartPosition<beginningPosition)
+            recentMessagesStartPosition =beginningPosition;
         consumer.seek(topicPartition, recentMessagesStartPosition);
 
         List<Message> messages = new ArrayList<>();
